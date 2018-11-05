ac831c150f0db217204bb3c9dbf2938f91d80d52
FixTheInternet <vmpatoleia@protonmail.com>
2018-11-05 11:22:12 +0100

    Express and body-parser setup

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+// Express setup
+const app = express()
+
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+
 function tester(funcs, args) {
 
     funcs.forEach(func => {
-        console.time("time")
+        const t1 = performance.now();
         for(var i = 0; i < 100000; i++ ){
             func(args);
         };
-        console.timeEnd("time");
+        const diff = performance.now() - t1;
+        console.log(diff);
     });
 }
 
